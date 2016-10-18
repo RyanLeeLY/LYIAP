ffd27f9ca40dda2eafbb3bf52e60ff5620ebbd1a
liyao <liyao1021@163.com>
2016-10-19 01:28:00 +0800

    Initial Commit

---
+++ LYIAP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LYIAP
+//
+//  Created by 李尧 on 2016/10/19.
+//  Copyright © 2016年 ryanleely. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
